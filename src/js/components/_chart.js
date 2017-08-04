c53abecd1845717a789af186f4170c910d6cd776
whats0n <mrdvv@mail.ru>
2017-08-04 11:37:58 +0300

    Add conditions to chart js

--- src/js/components/_chart.js
+++ src/js/components/_chart.js
@@ -2,76 +2,83 @@
 import Chart from 'chart.js';
 
 export default function() {
+	const categoryChart = document.getElementById('category-chart');
+	const productChart = document.getElementById('product-chart');
 
-	new Chart(document.getElementById('category-chart'), {
-		type: 'doughnut',
-		data: {
-			labels: [ 'Sandwich', 'Soft Drinks', 'Specials', 'Soup', 'Dessert', 'Pizza', 'Coffee', 'Salad' ],
-			datasets: [
-				{
-					backgroundColor: [
-						'rgb(192, 156, 220)',
-						'rgb(240, 93, 109)',
-						'rgb(64, 189, 173)',
-						'rgb(237, 213, 116)',
-						'rgb(232, 171, 171)',
-						'rgb(237, 213, 116)',
-						'rgb(254, 150, 99)',
-						'rgb(76, 171, 112)'
-					],
-					data: [20, 20, 30, 10, 5, 5, 5, 5]
+	if (categoryChart) {
+		new Chart(categoryChart, {
+			type: 'doughnut',
+			data: {
+				labels: [ 'Sandwich', 'Soft Drinks', 'Specials', 'Soup', 'Dessert', 'Pizza', 'Coffee', 'Salad' ],
+				datasets: [
+					{
+						backgroundColor: [
+							'rgb(192, 156, 220)',
+							'rgb(240, 93, 109)',
+							'rgb(64, 189, 173)',
+							'rgb(237, 213, 116)',
+							'rgb(232, 171, 171)',
+							'rgb(237, 213, 116)',
+							'rgb(254, 150, 99)',
+							'rgb(76, 171, 112)'
+						],
+						data: [20, 20, 30, 10, 5, 5, 5, 5]
+					}
+				]
+			},
+			options: {
+				cutoutPercentage: 70,
+				legend: {
+					display: false
 				}
-			]
-		},
-		options: {
-			cutoutPercentage: 70,
-			legend: {
-				display: false
 			}
-		}
-	});
+		});
+	}
 
-	new Chart(document.getElementById('product-chart'), {
-		type: 'doughnut',
-		data: {
-			labels: [ 
-				'Cheeseburger', 
-				'Ciabatta with chicken', 
-				'Ciabatta with tuna', 
-				'Ciabatta with spinach', 
-				'Sandwich with salmon', 
-				'Sandwich with ham', 
-				'Sandwich with broccoli', 
-				'Club sandwich', 
-				'Hamburger', 
-				'Sprite', 
-				'Pepsi'
-			],
-			datasets: [
-				{
-					backgroundColor: [
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(192, 156, 220)',
-						'rgb(240, 93, 109)',
-						'rgb(240, 93, 109)'
-					],
-					data: [ 10, 9, 9, 7, 7, 7, 6, 12, 13, 15, 5 ]
+	if (productChart) {
+		new Chart(productChart, {
+			type: 'doughnut',
+			data: {
+				labels: [ 
+					'Cheeseburger', 
+					'Ciabatta with chicken', 
+					'Ciabatta with tuna', 
+					'Ciabatta with spinach', 
+					'Sandwich with salmon', 
+					'Sandwich with ham', 
+					'Sandwich with broccoli', 
+					'Club sandwich', 
+					'Hamburger', 
+					'Sprite', 
+					'Pepsi'
+				],
+				datasets: [
+					{
+						backgroundColor: [
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(192, 156, 220)',
+							'rgb(240, 93, 109)',
+							'rgb(240, 93, 109)'
+						],
+						data: [ 10, 9, 9, 7, 7, 7, 6, 12, 13, 15, 5 ]
+					}
+				]
+			},
+			options: {
+				cutoutPercentage: 70,
+				legend: {
+					display: false
 				}
-			]
-		},
-		options: {
-			cutoutPercentage: 70,
-			legend: {
-				display: false
 			}
-		}
-	});
+		});
+	}
+
 
 };
