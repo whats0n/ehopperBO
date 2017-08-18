7f8cc9c6fb95cd5099746a5531ad70ed8baa0ee7
whats0n <mrdvv@mail.ru>
2017-08-18 11:14:27 +0300

    Updated resizable

--- src/js/components/_resizable-layout.js
+++ src/js/components/_resizable-layout.js
@@ -27,97 +27,95 @@ export default function() {
 			let slideBackBtnMargin = null;
 			let slideBackBtnFlag = false;
 
-			!touchFlag && $rightColumn.resizable({
-				resizeHeight: false,
-				handleSelector: '> .js-resizable-width-splitter',
-				resizeWidthFrom: 'left',
-				onDrag(event, $column, newWidth, newHeight, options) {
-					const rowWidth = $this.innerWidth();
-					const leftColumnWidth = `${rowWidth - newWidth}px`;
-
-					if ($headerTitle.length) {
-						const headerTitleWidth = $headerTitle.outerWidth();
-						const path = (headerTitleWidth - (rowWidth - newWidth));
-
-						if (rowWidth - newWidth < headerTitleWidth ) {
-							if (path <= headerTitleThreshold) {
-								headerTitleFlag = true;
-								headerTitleOpacity = (1 - (path / 100)).toFixed(2);
-								$headerTitle.css('opacity', headerTitleOpacity);
-							} else if (path > headerTitleThreshold && headerTitleOpacity != 0) {
-								headerTitleFlag = false;
-								headerTitleOpacity = 0;
-								$headerTitle.css('opacity', headerTitleOpacity);
-							}
-						} 
-
-						if (rowWidth - newWidth >= headerTitleWidth) {
-							if (!headerTitleFlag && headerTitleOpacity != 1) {
-								headerTitleFlag = true;
-							}
-							headerTitleOpacity = 1;
-						}
-
-						if (headerTitleFlag) {
-							$headerTitle.css('opacity', headerTitleOpacity);
-							headerTitleFlag = false;
-						}
+			if (touchFlag) {
+				$headerTitle
+					.add($rightColumn)
+					.addClass(ANIMATION);
+				$splitter.on('click', e => {
+					if ($splitter.hasClass(ACTIVE)) {
+						$splitter.removeClass(ACTIVE);
+						$headerTitle.removeClass(HIDDEN);
+						$slideBackBtn
+							.add($rightColumn)
+							.removeAttr('style');
+					} else {
+						$splitter.addClass(ACTIVE);
+						$headerTitle.addClass(HIDDEN);
+						$slideBackBtn.css('margin-left', $navBtn.outerWidth());
+						$rightColumn.css('width', '100%');
 					}
+				});
+			} else {
+				$rightColumn.resizable({
+					resizeHeight: false,
+					handleSelector: '> .js-resizable-width-splitter',
+					resizeWidthFrom: 'left',
+					onDrag(event, $column, newWidth, newHeight, options) {
+						const rowWidth = $this.innerWidth();
+						const leftColumnWidth = `${rowWidth - newWidth}px`;
 
-					if ($navBtn.length && $slideBackBtn.length) {
-						const navBtnWidth = $navBtn.outerWidth();
-						
-						if (rowWidth - newWidth <= navBtnWidth) {
-							const path = navBtnWidth - (rowWidth - newWidth);
-							slideBackBtnMargin = path < navBtnWidth ? path : navBtnWidth;
-							slideBackBtnFlag = true;
-						} else {
-							if (!slideBackBtnFlag && slideBackBtnMargin != 0) {
-								slideBackBtnFlag = true;
+						if ($headerTitle.length) {
+							const headerTitleWidth = $headerTitle.outerWidth();
+							const path = (headerTitleWidth - (rowWidth - newWidth));
+
+							if (rowWidth - newWidth < headerTitleWidth ) {
+								if (path <= headerTitleThreshold) {
+									headerTitleFlag = true;
+									headerTitleOpacity = (1 - (path / 100)).toFixed(2);
+									$headerTitle.css('opacity', headerTitleOpacity);
+								} else if (path > headerTitleThreshold && headerTitleOpacity != 0) {
+									headerTitleFlag = false;
+									headerTitleOpacity = 0;
+									$headerTitle.css('opacity', headerTitleOpacity);
+								}
+							} 
+
+							if (rowWidth - newWidth >= headerTitleWidth) {
+								if (!headerTitleFlag && headerTitleOpacity != 1) {
+									headerTitleFlag = true;
+								}
+								headerTitleOpacity = 1;
 							}
-							slideBackBtnMargin = 0;
+
+							if (headerTitleFlag) {
+								$headerTitle.css('opacity', headerTitleOpacity);
+								headerTitleFlag = false;
+							}
 						}
 
-						if (slideBackBtnFlag) {
-							$slideBackBtn.css('margin-left', slideBackBtnMargin);
-							slideBackBtnFlag = false;
+						if ($navBtn.length && $slideBackBtn.length) {
+							const navBtnWidth = $navBtn.outerWidth();
+							
+							if (rowWidth - newWidth <= navBtnWidth) {
+								const path = navBtnWidth - (rowWidth - newWidth);
+								slideBackBtnMargin = path < navBtnWidth ? path : navBtnWidth;
+								slideBackBtnFlag = true;
+							} else {
+								if (!slideBackBtnFlag && slideBackBtnMargin != 0) {
+									slideBackBtnFlag = true;
+								}
+								slideBackBtnMargin = 0;
+							}
+
+							if (slideBackBtnFlag) {
+								$slideBackBtn.css('margin-left', slideBackBtnMargin);
+								slideBackBtnFlag = false;
+							};
 						};
-					};
-					
-					// $rightColumn.css('max-width', `calc(100% - ${navBtnWidth}px)`);
-					$leftColumn.width(leftColumnWidth);
-				},
-				onDragEnd(event, $column, options) {
-					const rowWidth = $this.innerWidth();
-					const rightColumnWidth = `${$rightColumn.outerWidth() / rowWidth * 100}%`;
-					const leftColumnWidth = `${$leftColumn.outerWidth() / rowWidth * 100}%`;
-					const navBtnWidth = $navBtn.outerWidth();
+						
+						$leftColumn.width(leftColumnWidth);
+					},
+					onDragEnd(event, $column, options) {
+						const rowWidth = $this.innerWidth();
+						const rightColumnWidth = `${$rightColumn.outerWidth() / rowWidth * 100}%`;
+						const leftColumnWidth = `${$leftColumn.outerWidth() / rowWidth * 100}%`;
+						const navBtnWidth = $navBtn.outerWidth();
 
-					// $rightColumn.css('max-width', `calc(100% - ${navBtnWidth}px)`);
-					$rightColumn.width(rightColumnWidth);
-					$leftColumn.width(leftColumnWidth);
-				}
-			});
-
-			touchFlag && $splitter.on('click', e => {
-				if ($splitter.hasClass(ACTIVE)) {
-					$splitter.removeClass(ACTIVE);
-					$headerTitle
-						.removeClass(HIDDEN);
-					$slideBackBtn.removeAttr('style');
-					$rightColumn
-						.removeAttr('style');
-				} else {
-					$splitter.addClass(ACTIVE);
-					$headerTitle
-						.addClass(ANIMATION)
-						.addClass(HIDDEN);
-					$slideBackBtn.css('margin-left', $navBtn.outerWidth());
-					$rightColumn
-						.addClass(ANIMATION)
-						.css('width', '100%');
-				}
-			});
+						$rightColumn.width(rightColumnWidth);
+						$leftColumn.width(leftColumnWidth);
+					}
+				});
+			}
 			
 		});
 
