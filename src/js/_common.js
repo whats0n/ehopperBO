ee4ca66ea850c4d3ddca5c11220904da1a8810fb
whats0n <mrdvv@mail.ru>
2017-08-18 14:54:19 +0300

    Update body min height script and product display size on adaptive and touch

--- src/js/_common.js
+++ src/js/_common.js
@@ -6,38 +6,13 @@ export default (function() {
 		touch && $BODY.addClass(touchClass);
 		!touch && $BODY.addClass(noTouchClass);
 		if (touch) {
-			const width = window.outerWidth;
-			const height = window.outerHiehgt;
-			const screenWidth = window.screen.width;
+			const windowHeight = window.outerHeight;
 			const screenHeight = window.screen.height;
-			const barsHeight = screenHeight - height;
-			const PORTRAIT = 'portrait';
-			const LANDSCAPE = 'landscape';
-
-			//alert(screenWidth);
-			//alert(screenHeight);
-			//$BODY.css('min-height', $BODY.outerHeight());
+			const barsHeight = screenHeight - windowHeight;
 
 			window.addEventListener('orientationchange', (e) => {
-				const angle = screen.orientation.angle;
-				let orientation = null;
-				console.log(screen.orientation);
-				if (angle === 0 || angle === 180) orientation = LANDSCAPE;
-				if (angle === 90 || angle === 270) orientation = PORTRAIT;
-
-				switch(orientation) {
-					case PORTRAIT:
-						//$BODY.css('min-height', )
-						break;
-					case LANDSCAPE: 
-						break;
-				}
-
-				//$BODY.css('min-height', 1);
-				//setTimeout(() => {
-				//	$BODY.css('min-height', $BODY.outerHeight());
-				//}, 1000);
-				//alert(orientation);
+				const currentHeight = screen.height - barsHeight;
+				$BODY.css('min-height', currentHeight);
 			});
 		} 
 	});
